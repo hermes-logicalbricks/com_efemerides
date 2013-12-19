037edb1aba27f09b6546cd635985390117fe7c00
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2013-12-19 01:11:10 -0600

    Added version for Joomla 2.5, WIP

---
+++ com_efemerides/site/controller.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package    Efemerides
+ * @subpackage Components
+ * @link http://revolucionemosoaxaca.org
+ * @license    GNU/GPL
+ */
+
+// no direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * Efemerides Component Controller
+ *
+ * @package    Efemerides
+ * @subpackage Components
+ */
+class EfemeridesController extends JController
+{
+}
