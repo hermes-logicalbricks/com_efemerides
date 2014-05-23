4ed1d99c7cbb9d4920bd71ea2319f9af94235e88
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-05-23 13:44:02 -0500

    Cleaning the repository to start the development for Joomla 3.x component

--- com_efemerides/administrator/views/backup/tmpl/form.php
+++
@@ -1,7 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); 
-define( 'YOURBASEPATH', dirname(__FILE__) );
-
-echo "Esto esta funcionando";
-?>
-
-
--- com_efemerides/administrator/views/editcss/tmpl/form.php
+++
@@ -1,5 +0,0 @@
-<?php defined('_JEXEC') or die('Restricted access'); 
-define( 'YOURBASEPATH', dirname(__FILE__) );
-?>
-
-
